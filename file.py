d2b397b3520b0a6057aad7eb7d5a5e3dd853de35
moonwake769 <exhilaratedreverie288@protonmail.ch>
2021-11-17 21:41:33 +0100

    Update

--- file.py
+++ file.py
@@ -1,78 +1,85 @@
 import json
+import csv
 import os
 
 class File():
     """File object."""
     def __init__(
-        self, FILE_NAME
+        self, #FILE_NAME
     ):
-        self.file = FILE_NAME
+        #self.file = FILE_NAME
         # GETing Curent Working Directory.
         self.PATH = os.getcwd()
 
-    def is_exist(self) -> bool:
+    def is_exist(self, _pathToFile) -> bool:
         """Return True if file exists, otherwise False."""
-        if self.file in os.listdir(self.PATH):
+        if os.path.exists(_pathToFile): #in os.listdir(self.PATH):
             return True # file exists.
         else:
             return False # file does not exist.
 
-    def create(self):
-        """If file exists - pass, otherwise create it."""
-        if File.is_exist(self):
-            pass # Checked: found file.
-        else:
-            open(self.file, "w") # Created new file.
+    def create(self, _pathToFile):
+        """
+        If file does not exist - create it, otherwise return an Error.
+        A string argument should not contain ~ or $HOME due to error.
+        """
+        try:
+            open(_pathToFile, "x")
+        except FileExistsError:
+            raise "File already exists!"
 
-    def delete(self):
+    def delete(self, _pathToFile):
         """Delete file from computer permanently."""
-        os.system(f"rm {self.file}")
+        if os.path.exists(_pathToFile):
+            os.remove(_pathToFile)
+        else:
+            raise "File does not exist!"
 
-    def is_json(self) -> bool:
+    def is_json(self, _pathToFile) -> bool:
         """Return True if file is *.json, otherwise False."""
         try:
-            with open(self.file, "r+") as file:
+            with open(_pathToFile, "r+") as file:
                 json.load(file)
         except json.decoder.JSONDecodeError:
             return False # file is not *.json.
         else:
             return True # file is *.json.
 
-    def is_empty(self) -> bool:
+    def is_empty(self, _pathToFile) -> bool:
         """Return True if file is empty, False if not, None if file not exist."""
         try:
-            if os.path.isfile(self.file) and os.path.getsize(self.file) == 0:
+            if os.path.isfile(_pathToFile) and os.path.getsize(_pathToFile) == 0:
                 return True # file is empty.
         except FileNotFoundError:
             return None # file does not exist.
-        if os.path.isfile(self.file) and os.path.getsize(self.file) > 0:
+        if os.path.isfile(_pathToFile) and os.path.getsize(_pathToFile) > 0:
             return False # file is not empty.
 
-    def dump_to_json(self, _data:dict, _indent:int=None):
+    def dump_to_json(self, _data:dict, _pathToFile:str, _indent:int=4):
         """Dump data to *.json file."""
-        with open(self.file, "r+") as file:
+        with open(_pathToFile, "r+") as file:
             json.dump(_data, file, indent=_indent)
 
-    def dump_template_to_json(self, _template:dict, _indent:int=None):
+    def dump_template_to_json(self, _pathToFile:str, _template:dict, _indent:int=4):
         """Dump template if file is not empty."""
-        if is_empty() == False:
+        if is_empty(_pathToFile) == False:
             pass # file is not empty.
         else:
             # Creating template for *.json file.
-            with open(self.file, "r+") as file:
+            with open(_pathToFile, "r+") as file:
                 json.dump(_template, file, indent=_indent)
 
-    def load_from_json(self) -> dict:
+    def load_from_json(_pathToFile:str) -> dict:
         """"""
-        with open(self.file, "r+") as file:
+        with open(_pathToFile, "r+") as file:
             data = json.load(file)
         return data
-    
-    def convert_from_csv_to_json(self, _csvFilePath, _jsonFilePath, _primaryColumn):
+
+    def convert_from_csv_to_json(self, _pathToCSV, _pathToJSON, _primaryColumn):
         """Convert CSV file to JSON."""
         data = dict()
         # Open CSV reader.
-        with open(_csvFilePath, encoding="utf-8") as csvFile:
+        with open(_pathToCSV, encoding="utf-8") as csvFile:
             csv = csv.DictReader(csvFile)
             # Convert each row into a dictionary and add it to json.
             for row in csv:
