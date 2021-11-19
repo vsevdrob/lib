b79cef4e7675b1066e09f5bc19d5ddc95ccad321
moonwake769 <exhilaratedreverie288@protonmail.ch>
2021-11-20 00:43:31 +0100

    Update

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ import json
 import yaml
 import csv
 import os
-import PIL
+from PIL import Image as Image_
 
 class File():
     """File object."""
@@ -183,7 +183,7 @@ class Image(File):
         for file in f:
             for image_file_format in image_file_formats:
                 if file.endswith("." + image_file_format):
-                    image = PIL.Image.open(file)
+                    image = Image_.open(file)
                     data = list(image.getdata())
                     image_without_exif = Image.new(image.mode, image.size)
                     image_without_exif.putdata(data)
